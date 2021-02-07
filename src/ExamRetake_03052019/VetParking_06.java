da02bc8c4b401def33673f839d501e01d4a4dda2
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-07 17:29:48 +0200

    new files

--- src/ExamRetake_03052019/VetParking_06.java
+++ src/ExamRetake_03052019/VetParking_06.java
@@ -1,5 +1,29 @@
 package ExamRetake_03052019;
 
-public class VetParking_06 {
+import java.util.Scanner;
 
+public class VetParking_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = Integer.parseInt(scanner.nextLine());
+        int hours = Integer.parseInt(scanner.nextLine());
+        double sum ;
+        double total = 0;
+        for (int i = 1; i <= days; i++) {
+            sum=0;
+            for (int j = 1; j <= hours; j++) {
+                if (i % 2 == 0 && j % 2 != 0) {
+                    sum += 2.5;
+                } else if (i % 2 != 0 && j % 2 == 0) {
+                    sum += 1.25;
+                } else {
+                    sum += 1;
+                }
+            }
+                total += sum;
+                System.out.printf("Day: %d - %.2f leva%n", i, sum);
+
+        }
+        System.out.printf("Total: %.2f leva",total);
+    }
 }
