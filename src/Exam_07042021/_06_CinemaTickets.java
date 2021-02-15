48e095864902ed2ea49d36c7f7a51aa95583491f
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-15 09:19:31 +0200

    new task

---
+++ src/Exam_07042021/_06_CinemaTickets.java
@@ -0,0 +1,10 @@
+package Exam_07042021;
+
+import java.util.Scanner;
+
+public class _06_CinemaTickets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
