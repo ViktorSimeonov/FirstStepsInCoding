a5596b643115f256939114cd538996dcbc4a2ac6
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-15 09:19:53 +0200

    new task

---
+++ src/ExamRetake_03052019/_3_MobileOperator.java
@@ -0,0 +1,9 @@
+package ExamRetake_03052019;
+
+import java.util.Scanner;
+
+public class _3_MobileOperator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
